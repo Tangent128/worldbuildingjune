379a6d226138e040dfba34c44a5bb0ccbbd25079
Tangent 128 <Tangent128@gmail.com>
2016-06-11 20:34:29 -0400

    Types for Markov learning?

--- markov.ts
+++ markov.ts
@@ -1,6 +1,44 @@
 
-import { query, onclick } from 'lib';
+import { onclick } from 'lib';
+
+interface StateMap { [state: string] : State };
+
+class State {
+	nextWeights: { [state: string] : number } = {};
+};
+
+class Markovator {
+	states: StateMap = {};
+
+	private startState;
+	private endState;
+
+	constructor() {
+		this.startState = this.grabState("START");
+		this.endState = this.grabState("END");
+	};
+
+	grabState(symbol: string) {
+		let state = this.states[symbol];
+
+		if(state == null) {
+			state = new State();
+			this.states[symbol] = state;
+		}
+
+		return state;
+	};
+
+	learn(word: string) {
+		let state = this.startState;
+
+	};
+};
 
 onclick("#generateBtn", function() {
-	window.alert("test");
+	let engine = new Markovator();
+
+	engine.learn("Cool");
+
+	console.log(engine);
 });
